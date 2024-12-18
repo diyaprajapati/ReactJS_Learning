b02b077cbf6586cbd59cef4e44d147052e1e003d
diyaprajapati <diyabprajapati2005@gmail.com>
2024-12-18 23:09:17 +0530

    defined types of todo using zod

--- todo-app/backend/index.js
+++ todo-app/backend/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const { createTodo } = require('./types');
 const app = express();
 
-app.get('/', () => {
-    
+app.use(express.json());
+
+app.post('/todo', (req, res) => {
+    const createPayLoad = req.body;
+    const parsedPayLoad = createTodo.safeParse(createPayLoad);
+
+    if(!parsedPayLoad.success) {
+        req.status(411).json({
+            msg:"You sent the wrong inputs",
+        })
+        return;
+    }
+    //put it in mongodb
+})
+
+app.get('/todo', (req, res) => {
+
+})
+
+app.put('/completed', (req, res) => {
+
 })
 
 app.listen(3000, () => {
